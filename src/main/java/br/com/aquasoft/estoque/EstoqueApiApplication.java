f59721ffb7a5e3ca6c56cf8951913d288dc16a0e
Gabs Monteiro <gabsdocompiuter@gmail.com>
2026-05-09 01:01:56 -0300

    Inicializa projeto

---
+++ src/main/java/br/com/aquasoft/estoque/EstoqueApiApplication.java
@@ -0,0 +1,18 @@
+package br.com.aquasoft.estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@SpringBootApplication
+public class EstoqueApiApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueApiApplication.class, args);
+	}
+
+}

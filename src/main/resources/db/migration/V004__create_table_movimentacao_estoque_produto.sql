f1a019c28c0b5feca5f30c7376202f3de92c7ef4
Gabs Monteiro <gabsdocompiuter@gmail.com>
2026-05-10 10:43:37 -0300

    Adiciona flyway

--- src/main/resources/db/migration/V004__create_table_movimentacao_estoque_produto.sql
+++ src/main/resources/db/migration/V004__create_table_movimentacao_estoque_produto.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS movimentacao_estoque_produto (
     id BIGSERIAL PRIMARY KEY,
     estoque_produto_id BIGINT NOT NULL,
     quantidade DECIMAL(13, 2) NOT NULL,
-    horario_movimentacao DATE NOT NULL,
+    horario_movimentacao TIMESTAMP NOT NULL,
     tipo_movimentacao_estoque VARCHAR(20) NOT NULL,
 
     constraint fk_movimentacao_estoque_produto_estoque_produto
